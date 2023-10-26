6119b0124433c9985ac12a5122d4577f553fabd8
Facundo Ramallo <facu.ramallo@gmail.com>
2023-10-26 13:08:12 -0300

    funciones pacient modificasdas, funciones practicas empezadas

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #define clear system("cls")
 #define pausa system("pause")
 
-typedef struct empleados_laboratorio {
+typedef struct empleados_laboratorio {///USUARIOS QUE VAN A LOGUEAR
     int DNI;
     char NyA[40];
     char Usuario[20];
@@ -14,7 +14,7 @@ typedef struct empleados_laboratorio {
     char perfil[20];
 } empleados_laboratorio;
 
-typedef struct paciente {
+typedef struct paciente {/// ARBOL
     char NyA[40];
     int Edad;
     int DNI;
@@ -26,14 +26,14 @@ typedef struct paciente {
     struct paciente *der; // Puntero al hijo derecho en el árbol (mayor DNI)
 } paciente;
 
-typedef struct ingresos {
+typedef struct ingresos {///PRIMERA LISTA QUE SE DESPRENDE DEL ARBOL DE PACIENTES
     int NroDeIngreso;
     char FechaDeIngreso[10];
     char FechaDeRetiro[10];
     int DniPaciente;
     int MatriculaDelProfesionalSolicitante;
     int Eliminado;
-    struct practicas *listaPracticas; // Puntero a la lista de prácticas asociadas a este ingreso
+    struct pracXingreso *listaPracticasXingreso; // Puntero a la lista de prácticas asociadas a este ingreso
     struct ingresos *siguiente; // Puntero al siguiente ingreso en la lista de ingresos
 } ingresos;
 
@@ -43,24 +43,35 @@ typedef struct practicas {
     int Eliminado;
 } practicas;
 
-typedef struct pracXingreso {
+typedef struct pracXingreso {///SEGUNDA LISTA QUE SE DESPRENDE DE LA LISTA DE INGRESOS
     int NroDeIngreso;
     int NroDePractica;
     char Resultado[40];
-    struct pracXingreso *siguiente; // Puntero al siguiente elemento en la lista de prácticas por ingreso
+    struct pracXingreso *siguiente;
 } pracXingreso;
 
 int main() {
 
+    /**
     altaEmpleado();
-    mostrarEmpleados();
+    mostrarEmpleadosEnArchivo();
     borrarEmpleadoPorDNI();
-    mostrarEmpleados();
+    modificarEmpleadoPorDNI();
+    mostrarEmpleadosEnArchivo(); **/
+
+    /**altaPaciente();
+    mostrarPacientes();
+    modificarPacientePorDNI();**/
+
+    /**guardarPracticasEnArchivo();
+    mostrarPracticas**/
+
+
     return 0;
 }
 
 
-/// ARCHIVO EMPLEADOS
+/// FUNCIONES PARA ARCHIVO EMPLEADOS
 empleados_laboratorio ingresoEmpleado() { /// INGRESO EMPLEADO POR CONSOLA
     empleados_laboratorio nuevo;
     char comprobacion[20]; // Comprobación para la contraseña
@@ -158,7 +169,7 @@ void altaEmpleado() {/// PIDE DNI, COMPRUEBA QUE NO EXISTA Y COMIENZA EL ALTA. U
         printf("\nEL EMPLEADO YA EXISTE EN EL ARCHIVO.");
         return;
     }else{
-        printf("\nNUEVA ALTA DE EMPLEADO: ");
+        printf("\nNUEVA ALTA DE EMPLEADO: \n");
     }
 
     FILE *archi = fopen("empleados.bin", "ab");
@@ -177,7 +188,7 @@ void altaEmpleado() {/// PIDE DNI, COMPRUEBA QUE NO EXISTA Y COMIENZA EL ALTA. U
     fclose(archi);
 }
 
-void mostrarEmpleados() {/// MUESTRA EMPLEADOS CARGADOS EN EL ARCHIVO EMPLEADOS.BIN
+void mostrarEmpleadosEnArchivo() {/// MUESTRA EMPLEADOS CARGADOS EN EL ARCHIVO EMPLEADOS.BIN
     FILE *archi = fopen("empleados.bin", "rb");
     if (archi == NULL) {
         printf("\n ERROR AL ABRIR EL ARCHIVO.");
@@ -189,10 +200,11 @@ void mostrarEmpleados() {/// MUESTRA EMPLEADOS CARGADOS EN EL ARCHIVO EMPLEADOS.
     while (fread(&empleado, sizeof(empleados_laboratorio), 1, archi) == 1) {
         contador++;
         printf("\n%d)", contador);
-        printf("NOMBRE Y APELLIDO: %s, ", empleado.NyA);
-        printf("DNI: %d, ", empleado.DNI);
-        printf("USUARIO: %s, ", empleado.Usuario);
-        printf("PERFIL: %s\n", empleado.perfil);
+        printf("\nNOMBRE Y APELLIDO: %s", empleado.NyA);
+        printf("\nDNI: %d", empleado.DNI);
+        printf("\nUSUARIO: %s", empleado.Usuario);
+        printf("\nPERFIL: %s", empleado.perfil);
+        printf("\nCONTASENA: %s", empleado.constrasena);
         printf("\n");
     }
     if (contador == 0) {
@@ -242,5 +254,324 @@ void borrarEmpleadoPorDNI() {///BORRA EMPLEADOS POR DNI EN EL ARCHIVOS EMPLEADOS
     } else {
         remove("temp.bin"); // Si no se encontró al empleado, eliminamos el archivo temporal
         printf("Empleado con DNI %d no encontrado.\n", dni);
+    }
+}
+
+void modificarEmpleadoPorDNI() {///PIDE DNI, BUSCA EN EL ARCHIVO EMPLEADOS.BIN Y MODIFICA LOS DATOS DEL EMPLEADO ENCONTRADO
+    int dni;
+    printf("\nINGRESE EL DNI DEL EMPLEADO: ");
+    scanf("%d", &dni);
+
+    FILE *archi = fopen("empleados.bin", "rb+");//rb+ permite la modificacion de los registros existentes.
+    if (archi == NULL) {
+        printf("\nERROR AL ABRIR EL ARCHIVO DE EMPLEADOS.");
+        return;
+    }
+
+    empleados_laboratorio empleado;
+    int encontrado = 0; // flag
+
+    while (fread(&empleado, sizeof(empleados_laboratorio), 1, archi) == 1) {
+        if (empleado.DNI == dni) {
+            encontrado = 1;
+            printf("Empleado encontrado:\n");
+            printf("Nombre y Apellido: %s\n", empleado.NyA);
+            printf("DNI: %d\n", empleado.DNI);
+            printf("Perfil: %s\n", empleado.perfil);
+            printf("Nombre de usuario: %s\n", empleado.Usuario);
+            printf("Contraseña: %s\n", empleado.constrasena);
+
+            // Realizar modificaciones
+            printf("Ingrese las nuevas informaciones:\n");
+            printf("Nombre y Apellido: ");
+            fflush(stdin);
+            gets(empleado.NyA);
+            printf("Perfil: ");
+            fflush(stdin);
+            gets(empleado.perfil);
+            printf("Nombre de usuario: ");
+            fflush(stdin);
+            gets(empleado.Usuario);
+            printf("Contraseña: ");
+            fflush(stdin);
+            scanf("%s", empleado.constrasena);
+
+            rewind(archi);
+
+            if (fwrite(&empleado, sizeof(empleados_laboratorio), 1, archi) != 1) {
+                printf("\nERROR AL ESCRIBIR EN EL ARCHIVO.");
+            } else {
+                printf("\nEMPLEADO MODIFICADO CON EXITO.");
+            }
+        }
+    }
+
+    fclose(archi);
+
+    if (!encontrado) {
+        printf("EMPLEADO DNI %d NO ENCONTRADO.\n", dni);
+    }
+}
+
+
+/// FUNCIONES PARA ARCHIVO PACIENTES
+paciente IngresoPaciente() {///INGRESA DATOS DE PACIENTES E INICIALIZA LOS PUNTEROS EN NULL.
+    paciente nuevo;
+    printf("\nAPELLIDO Y NOMBRE DEL PACIENTE: ");
+    fflush(stdin);
+    gets(&nuevo.NyA);
+    printf("\nEDAD DEL PACIENTE: ");
+    scanf(" %d", &nuevo.Edad);
+    printf("\nDNI DEL PACIENTE: ");
+    scanf(" %d", &nuevo.DNI);
+    printf("\nDIRECCION DEL PACIENTE: ");
+    fflush(stdin);
+    gets(&nuevo.Direccion);
+    printf("\nTELEFONO DEL PACIENTE: ");
+    fflush(stdin);
+    gets(&nuevo.telefono);
+    nuevo.Eliminado = 0; // Por defecto, el paciente no esta dado de baja
+
+    nuevo.listaIngresos = NULL;
+    nuevo.izq = NULL;
+    nuevo.der = NULL;
+
+    return nuevo;
+}
+
+bool pacienteExiste(int dni) {/// TRUE OR FALSE, SI EL PACIENTE EXISTE O NO EN EL ARCHIVO PACIENTES.BIN, SE USA EN ALTA PACIENTE.
+    FILE *archi = fopen("pacientes.bin", "rb");
+    if (archi == NULL) {
+        return false; // Si no se puede abrir el archivo, asumimos que el paciente no existe
+    }
+
+    paciente paciente;
+    while (fread(&paciente, sizeof(paciente), 1, archi) == 1) {
+        if (paciente.DNI == dni) {
+            fclose(archi);
+            return true; // El paciente ya existe en el archivo
+        }
+    }
+
+    fclose(archi);
+    return false; // El paciente no existe en el archivo
+}
+
+void altaPaciente() {/// PIDE DNI, COMPRUEBA QUE NO EXISTA Y COMIENZA EL ALTA. UTILIZA INGRESO PACIENTE Y PACIENTE EXISTE
+    paciente nuevo;
+    int dni;
+
+    printf("\nDNI DEL PACIENTE: ");
+    scanf("%d", &dni);
+
+    if (pacienteExiste(dni)) {//quiere decir si paciente dni es "TRUE"...
+        printf("\nEL PACIENTE CON DNI %d YA EXISTE EN EL ARCHIVO.", dni);
+        return;
+    }
+
+    // en cambio si devuelve "FALSE", saltea el if.
+    nuevo = IngresoPaciente();
+
+    FILE *archi = fopen("pacientes.bin", "ab");
+    if (archi == NULL) {
+        printf("\nERROR AL ABRIR EL ARCHIVO.");
+        return;
+    }
+
+    if (fwrite(&nuevo, sizeof(paciente), 1, archi) != 1) {
+        printf("\nERROR AL ESCRIBIR EN EL ARCHIVO.");
+    } else {
+        printf("\nPACIENTE AGREGADO CON EXITO.");
+    }
+
+    fclose(archi);
+}
+
+void mostrarPacientes() {///MUESTRA PACIENTES POR DNI EN EL ARCHIVO DE PACIENTES.BIN
+    FILE *archi = fopen("pacientes.bin", "rb");
+    if (archi == NULL) {
+        printf("\nERROR AL ABRIR EL ARCHIVO DE PACIENTES");
+        return;
+    }
+
+    paciente paciente;
+    int contador = 0;
+
+    while (fread(&paciente, sizeof(paciente), 1, archi) == 1) {
+        contador++;
+        printf("\nPACIENTE #%d)", contador);
+        printf("\nNOMBRE Y APELLIDO: %s", paciente.NyA);
+        printf("\nEDAD: %d", paciente.Edad);
+        printf("\nDNI: %d", paciente.DNI);
+        printf("\nDIRECCION: %s", paciente.Direccion);
+        printf("\nTELEFONOS: %s", paciente.telefono);
+        printf("\nELIMINADO: %d", paciente.Eliminado);
+        printf("\n");
+    }
+
+    if (contador == 0) {
+        printf("No hay pacientes registrados en el archivo.\n");
+    }
+
+    fclose(archi);
+}
+
+void modificarPacientePorDNI() {///PIDE DNI, BUSCA EN EL ARCHIVO DE PACIENTES.BIN Y MODIFICA LOS DATOS DEL PACIENTE ENCONTRADO
+    int dni;
+    printf("\nINGRESE EL DNI DEL PACIENTE A MODIFICAR: ");
+    scanf("%d", &dni);
+
+    FILE *archi = fopen("pacientes.bin", "rb+");
+    if (archi == NULL) {
+        printf("\nERROR AL ABRIR EL ARCHIVO DE PACIENTES.");
+        return;
+    }
+
+    paciente nuevo;
+    int encontrado = 0; // Flag
+
+    while (fread(&nuevo, sizeof(paciente), 1, archi) == 1) {
+        if (nuevo.DNI == dni) {
+            encontrado = 1;
+            printf("\nPACIENTE ENCONTRADO:\n");
+            printf("\nNOMBRE Y APELLIDO: %s", nuevo.NyA);
+            printf("\nDNI: %d", nuevo.DNI);
+            printf("\nEDAD: %d", nuevo.Edad);
+            printf("\nTELEFONO: %s", nuevo.telefono);
+            printf("\nDIRECCION: %s", nuevo.Direccion);
+
+            // Realizar modificaciones
+            printf("\nINGRESE LA NUEVA INFORMACION:\n");
+            printf("\nNUEVO NOMBRE Y APELLIDO: ");
+            fflush(stdin);
+            gets(nuevo.NyA);
+            printf("\nNUEVO DNI: ");
+            scanf("%d", &nuevo.DNI);
+            printf("\nNUEVA EDAD: ");
+            scanf("%d", &nuevo.Edad);
+            printf("\nNUEVO TELEFONO: ");
+            fflush(stdin);
+            gets(nuevo.telefono);
+            printf("\nNUEVA DIRECCION: ");
+            fflush(stdin);
+            gets(nuevo.Direccion);
+
+            fseek(archi, -sizeof(paciente), SEEK_CUR); // Retrocede al inicio del registro como el rewind(archi)
+            fwrite(&nuevo, sizeof(paciente), 1, archi); // Sobrescribe el registro modificado
+            printf("\nPACIENTE MODIFICADO CON EXITO.\n");
+            break; /// Sal del bucle una vez que se haya modificado el paciente... PREGUNTARLE A ADRIAN SI PUEDO USARLO
+        }
+    }
+
+    fclose(archi);
+
+    if (!encontrado) {
+        printf("PACIENTE DNI %d NO ENCONTRADO.\n", dni);
+    }
+}
+//FALTA BAJA DE PACIENTE CON VERIFICACION DE INGRESOS ASOCIADOS.
+
+
+///FUNCIONES PARA ARCHIVO DE PRACTICAS
+practicas ingresoPracticas() {
+    practicas practica;
+
+    printf("\nNOMBRE DE PRACTICA: ");
+    fflush(stdin);
+    gets(practica.NombreDePractica);
+    printf("\nNro DE PRACTICA: ");
+    scanf(" %d", &practica.NroDePractica);
+    practica.Eliminado = 0;
+
+    return practica;
+}
+
+void guardarPracticasEnArchivo() {
+    FILE *archi = fopen("practicas.bin", "ab");
+    if (archi == NULL) {
+        printf("\nERROR AL ABRIR EL ARCHIVO DE PRACTICAS.");
+        return;
+    }
+
+    int numPracticas;
+    printf("\nINGRESE LA CANTIDAD DE PRACTICAS QUE DESEA INGRESAR: ");
+    scanf(" %d", &numPracticas);
+
+    for (int i = 0; i < numPracticas; i++) {
+        practicas practica = ingresoPracticas();
+
+        if (fwrite(&practica, sizeof(practicas), 1, archi) != 1) {
+            printf("\nERROR AL ESCRIBIR LA PRACTICA EN EL ARCHIVO.");
+        }
+    }
+
+    fclose(archi);
+    printf("\nSE GUARDARON %d PRACTICAS ",numPracticas);
+}
+
+void mostrarPracticas() {
+    FILE *archi = fopen("practicas.bin", "rb");
+    if (archi == NULL) {
+        printf("\n ERROR AL ABRIR EL ARCHIVO DE PRACTICAS.\n");
+        return;
+    }
+
+    practicas practica;
+    int contador = 0;
+
+    printf("\nPRACTICAS REGISTRADAS:\n");
+
+    while (fread(&practica, sizeof(practicas), 1, archi) == 1) {
+        contador++;
+        printf("\n%d) Nro DE PRACTICA: %d", contador, practica.NroDePractica);
+        printf("\n   NOMBRE DE PRACTICA: %s", practica.NombreDePractica);
+        //printf("\n   ESTADO: %s\n", (practica.Eliminado == 0) ? "Activo" : "Inactivo");
+        ///VER SI CON EL ESTADO 0 ESTA ACTIVO Y CON 1 DADO DE BAJA Y DEBE SER ELIMINNADO DEL ARCHIVO, O DEJARLO Y MOSTRAR QUE ESTA DADO DE BAJA PARA UN HISTORIAL.
+    }
+
+    if (contador == 0) {
+        printf("\nNO HAY PRACTICAS REGISTRADAS EN EL ARCHIVO.\n");
+    }
+
+    fclose(archi);
+}
+
+void modificarPracticaPorNumero() {
+    int numero;
+    printf("\nINGRESE EL NUMERO DE LA PRACTICA QUE DESEA ELIMINAR: ");
+    scanf(" %d", &numero);
+    FILE *archi = fopen("practicas.bin", "rb+");
+    if (archi == NULL) {
+        printf("\n ERROR AL ABRIR EL ARCHIVO DE PRACTICAS.\n");
+        return;
+    }
+
+    practicas practica;
+    int encontrado = 0;
+
+    while (fread(&practica, sizeof(practicas), 1, archi) == 1) {
+        if (practica.NroDePractica == numero) {
+            encontrado = 1;
+            printf("\nPRACTICA ENCONTRADA:\n");
+            printf("Nro DE PRACTICA: %d\n", practica.NroDePractica);
+            printf("NOMBRE DE PRACTICA: %s\n", practica.NombreDePractica);
+
+            // Realizar la modificación
+            printf("\nINGRESE EL NUEVO NOMBRE PARA LA PRACTICA: ");
+            fflush(stdin);
+            gets(practica.NombreDePractica);
+
+            // Regresar al inicio del registro para sobrescribirlo
+            rewind(archi);
+            fwrite(&practica, sizeof(practicas), 1, archi);
+            printf("\nPRACTICA MODIFICADA CON EXITO.\n");
+            break;
+        }
+    }
+
+    fclose(archi);
+
+    if (!encontrado) {
+        printf("\n PRACTICA CON Nro %d, NO ENCONTRADA.", numero);
     }
 }
